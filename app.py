137bbb5eb39166b60d134013330d46c2bc4e8384
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-11-08 12:35:41 +0100

    test test test fix

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from hashlib import sha256
+import base64
 
 # Set page config
 st.set_page_config(page_title="VA-Polisvoorwaardentool")

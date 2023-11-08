cd305911d426c88a510b72b9ef362aa36e61924a
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-11-08 13:22:16 +0100

    https test fix?

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ else:
 def display_pdf(file_path):
     with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="https://data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 # Global variable to cache embeddings to reduce repeated API calls

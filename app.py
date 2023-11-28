e7787b74003755252b16d467f538969ae02f7634
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-11-28 17:03:34 +0100

    test

--- app.py
+++ app.py
@@ -152,8 +152,8 @@ def main():
 
     user_question = st.text_input("Stel een vraag over de polisvoorwaarden")
     if user_question:
-        #prompt = PromptTemplate.from_template("Beantwoord de volgende vraag:{vraag})
-        #prompt.format(vraag="user_question")
+        prompt = PromptTemplate.from_template("Beantwoord de volgende vraag:{vraag})
+        prompt.format(vraag="user_question")
         docs = knowledge_base.similarity_search(prompt)  # Zoek het meest relevante deel van het document
     
 

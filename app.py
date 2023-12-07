e30fd95f202a400d6f03b885b8aa3ebdcc342fa5
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-12-07 13:46:42 +0100

    test

--- app.py
+++ app.py
@@ -21,21 +21,16 @@ from hashlib import sha256
 
 assistant_id = st.secrets["openai_assistant_id"]
 
-# Set page config
 st.set_page_config(page_title="VA-Polisvoorwaardentool")
 
-# Check password
 hashed_password = st.secrets["hashed_password"]
 password_input = st.text_input("Wachtwoord:", type="password")
 
 if sha256(password_input.encode()).hexdigest() != hashed_password:
     st.error("Voer het juiste wachtwoord in.")
     st.stop()
-
-# Global variable to cache embeddings to reduce repeated API calls
+    
 knowledge_bases = {}
-
-# Aan het begin van je script na het importeren van openai en het instellen van de API-sleutel
 
 def submit_message(assistant_id, thread_id, user_message):
     response = openai.Message.create(
@@ -51,84 +46,62 @@ def get_response(thread_id):
     response = openai.Thread.retrieve(thread_id=thread_id)
     return response
 
-def main():
-    # Je bestaande main() functie
 
-
-
+   
 def categorize_pdfs(pdf_list):
     category_map = {}
     for pdf in pdf_list:
         prefix = os.path.basename(pdf).split('_')[0]
-        if prefix == "Auto":
-            category = "Autoverzekering"
-        elif prefix == "AVB":
-            category = "Bedrijfsaansprakelijkheidsverzekering"
-        elif prefix == "BestAVB":
-            category = "AVB Bestuurders"
-        elif prefix == "BS":
-            category = "Bedrijfsschadeverzekering"
-        elif prefix == "BestAuto":
-            category = "Bestelautoverzekering"
-        elif prefix == "Brand":
-            category = "Brandverzekeringen"
-        elif prefix == "Cara":
-            category = "Caravanverzekering"
-        elif prefix == "EigVerv":
-            category = "Eigen vervoer"
-        elif prefix == "Fiets":
-            category = "Fietsverzekering"
-        elif prefix == "Geb":
-            category = "Gebouwen"
-        elif prefix == "GW":
-            category = "Goed Werkgeverschap"
-        elif prefix == "IB":
-            category = "Inboedelverzekering"
-        elif prefix == "Inv":
-            category = "Inventaris"
-        elif prefix == "Mot":
-            category = "Motorverzekering"
-        elif prefix == "RB":
-            category = "Rechtsbijstandverzekering"
-        elif prefix == "Reis":
-            category = "Reisverzekering"
-        elif prefix == "Scoot":
-            category = "Scootmobielverzekering"
-        elif prefix == "WEGAS":
-            category = "WEGAS"
-        elif prefix == "WerkMat":
-            category = "Werk- en landbouwmaterieelverzekering"
-        elif prefix == "WEGAM":
-            category = "Werkgeversaansprakelijkheid Motorrijtuigen (WEGAM)"
-        elif prefix == "Woon":
-            category = "Woonhuisverzekering"
-        else:
-            category = "Overige"
+            if prefix == "Auto":
+                category = "Autoverzekering"
+            elif prefix == "AVB":
+                category = "Bedrijfsaansprakelijkheidsverzekering"
+            elif prefix == "BestAVB":
+                category = "AVB Bestuurders"
+            elif prefix == "BS":
+                category = "Bedrijfsschadeverzekering"
+            elif prefix == "BestAuto":
+                category = "Bestelautoverzekering"
+            elif prefix == "Brand":
+               category = "Brandverzekeringen"
+            elif prefix == "Cara":
+                category = "Caravanverzekering"
+            elif prefix == "EigVerv":
+                category = "Eigen vervoer"
+            elif prefix == "Fiets":
+                category = "Fietsverzekering"
+            elif prefix == "Geb":
+                category = "Gebouwen"
+            elif prefix == "GW":
+                category = "Goed Werkgeverschap"
+            elif prefix == "IB":
+                category = "Inboedelverzekering"
+            elif prefix == "Inv":
+                category = "Inventaris"
+            elif prefix == "Mot":
+                category = "Motorverzekering"
+            elif prefix == "RB":
+                category = "Rechtsbijstandverzekering"
+            elif prefix == "Reis":
+                category = "Reisverzekering"
+            elif prefix == "Scoot":
+                category = "Scootmobielverzekering"
+            elif prefix == "WEGAS":
+                category = "WEGAS"
+            elif prefix == "WerkMat":
+                category = "Werk- en landbouwmaterieelverzekering"
+            elif prefix == "WEGAM":
+                category = "Werkgeversaansprakelijkheid Motorrijtuigen (WEGAM)"
+            elif prefix == "Woon":
+                category = "Woonhuisverzekering"
+            else:
+                category = "Overige"
 
         if category not in category_map:
             category_map[category] = []
         category_map[category].append(pdf)
 
     return category_map
-
-#def create_custom_prompt(user_question):
-    #custom_prompt = #(
-        #f"Dit document is een polisvoorwaardenblad. "
-        #f"De volgende vraag moet worden beantwoord door directe informatie uit dit document te gebruiken. "
-        #f"Bij het zoeken naar een antwoord, houd rekening met de volgende punten:\n"
-        #f"- Controleer of de vraag betrekking heeft op algemene voorwaarden, uitzonderingen, specifieke clausules, of dekkingslimieten.\n"
-        #f"- Zoek naar definities of specifieke termen die relevant zijn voor de vraag. Verzekeringsdocumenten gebruiken vaak specifiek gedefinieerde termen.\n"
-        #f"- Let op de context waarin termen worden gebruikt. Een term kan verschillende betekenissen hebben afhankelijk van de sectie waarin deze voorkomt.\n"
-        #f"- Als de vraag betrekking heeft op dekking, controleer dan zowel de secties over inbegrepen dekking als uitsluitingen.\n"
-        #f"- Geef een duidelijk antwoord ('Ja, het is gedekt', 'Nee, het is niet gedekt', of 'Niet van toepassing') alleen als je expliciet bewijs in het document vindt. "
-        #f"Leg uit waarom dit zo is, bijvoorbeeld 'Ja, het is gedekt, omdat in sectie 4, pagina 12, staat dat...'.\n"
-        #f"- Als het antwoord niet duidelijk in het document staat, of als het onderwerp wordt genoemd in een context die geen definitief antwoord geeft, geef dan aan met 'Antwoord niet expliciet gevonden in het document'.\n"
-        #f"- Vermijd het maken van aannames of het trekken van conclusies op basis van gerelateerde onderwerpen of algemene kennis.\n"
-        #f"- Geef indien mogelijk de specifieke sectie of pagina van het document aan waar het relevante antwoord gevonden kan worden.\n\n"
-        #f"Vraag: {user_question}\n"
-        #f"Antwoord:")#
-    #return custom_prompt
-
 
 def main():
     

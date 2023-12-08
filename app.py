0b7cc608c3174279a295321050de81429e0e16e0
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-12-08 17:06:32 +0100

    tyest

--- app.py
+++ app.py
@@ -126,15 +126,13 @@ def main():
     selected_pdf_path = available_pdfs[pdf_names.index(selected_pdf_name)]
 
     user_question = st.text_input("Stel een vraag over de polisvoorwaarden")
-
-
-        if selected_pdf_path:
-            with open(selected_pdf_path, "rb") as file:
-                st.download_button(
-                    label="Download polisvoorwaarden",
-                    data=file,
-                    file_name=selected_pdf_name,
-                    mime="application/pdf"
+    if selected_pdf_path:
+        with open(selected_pdf_path, "rb") as file:
+            st.download_button(
+                label="Download polisvoorwaarden",
+                data=file,
+                file_name=selected_pdf_name,
+                mime="application/pdf"
                 )
 
         if selected_pdf_path not in knowledge_bases:

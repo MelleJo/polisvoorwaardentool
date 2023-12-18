3ee2d82934a457b85dfc0a8e7d0ec8715d00ab78
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-12-18 16:37:06 +0000

    nieuwe versie

--- app.py
+++ app.py
@@ -10,29 +10,32 @@ from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplat
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.callbacks import get_openai_callback
 from hashlib import sha256
-
-prompt_template = ChatPromptTemplate.from_template(
-    "Je bent een expert in het interpreteren van verzekeringsdocumenten. "
-    "Bij het beantwoorden van vragen, gebruik de informatie uit de polisvoorwaarden. "
-    "De gebruiker is een schadebehandelaar, geef altijd zo nuttig mogelijk antwoord. Wordt er bijvoorbeeld naar een maximale vergoeding gevraagd? Geef dan enkel het bedrag en waar het staat."
-    "Geef een accurate beschrijving van waar de gebruiker de informatie/context die jij hebt gebruikt kan vinden. Zorg ervoor dat je niet liegt of hallucineert over waar dit staat."
-    "Vraag: {user_question}"
-)
+from langchain.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 
-# Set page config
+
+
+# webapp setup
+
+# titel enzovoorts
 st.set_page_config(page_title="VA-Polisvoorwaardentool")
 
-# Check password
+# wachtwoord
 hashed_password = st.secrets["hashed_password"]
 password_input = st.text_input("Wachtwoord:", type="password")
 
+# wachtwoord check
 if sha256(password_input.encode()).hexdigest() != hashed_password:
     st.error("Voer het juiste wachtwoord in.")
     st.stop()
 
+
+
 # Global variable to cache embeddings to reduce repeated API calls
 knowledge_bases = {}
+
+# pdf categorieen
 
 def categorize_pdfs(pdf_list):
     category_map = {}
@@ -90,81 +93,50 @@ def categorize_pdfs(pdf_list):
 
     return category_map
 
+
 def main():
     st.header("VA-Polisvoorwaardentool")
 
-    chat = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.2)
-    chain = prompt_template | chat
+    custom_prompt = (
+        "Je bent een expert in het interpreteren van verzekeringsdocumenten. "
+        "Bij het beantwoorden van vragen, gebruik de informatie uit de polisvoorwaarden. "
+        "De gebruiker is een schadebehandelaar, geef altijd zo nuttig mogelijk antwoord. "
+        "Wordt er bijvoorbeeld naar een maximale vergoeding gevraagd? Geef dan enkel het bedrag en waar het staat. "
+        "Geef een accurate beschrijving van waar de gebruiker de informatie/context die jij hebt gebruikt kan vinden. "
+        "Zorg ervoor dat je niet liegt of hallucineert over waar dit staat. "
+        "Vraag: {user_question}"
+    )
 
+    chain = (
+        {"user_question": RunnablePassthrough()}
+        | ChatPromptTemplate.from_template(custom_prompt)
+        | ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.2)
+        | StrOutputParser()
+    )
+
+    # Set API key for OpenAI functionalities
     api_key = st.secrets["OPENAI_API_KEY"]
     os.environ["OPENAI_API_KEY"] = api_key
 
-     
-    pdf_dir = "preloaded_pdfs/"
-    all_pdfs = [os.path.join(dp, f) for dp, dn, filenames in os.walk(pdf_dir) for f in filenames if f.endswith('.pdf')]
-
-    category_map = categorize_pdfs(all_pdfs)
-
-    categories = list(category_map.keys())
-    if not categories:
-        st.warning("Geen polisvoorwaarden gevonden.")
-        return
-
-    selected_category = st.selectbox("Kies een categorie:", categories)
-    available_pdfs = category_map[selected_category]
-    pdf_names = [os.path.basename(pdf) for pdf in available_pdfs]
-    selected_pdf_name = st.selectbox("Welke polisvoorwaarden wil je raadplegen?", pdf_names)
-    selected_pdf_path = available_pdfs[pdf_names.index(selected_pdf_name)]
-
-    if selected_pdf_path:
-        with open(selected_pdf_path, "rb") as file:
-            st.download_button(
-                label="Download polisvoorwaarden",
-                data=file,
-                file_name=selected_pdf_name,
-                mime="application/pdf"
-            )
-
-    if selected_pdf_path not in knowledge_bases:
-        with open(selected_pdf_path, "rb") as f:
-            pdf_reader = PdfReader(f)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            text_splitter = CharacterTextSplitter(separator="\n", chunk_size=4000, chunk_overlap=1000, length_function=len)
-            chunks = text_splitter.split_text(text)
-            embeddings = OpenAIEmbeddings()
-            knowledge_bases[selected_pdf_path] = FAISS.from_texts(chunks, embeddings)
-
-    knowledge_base = knowledge_bases[selected_pdf_path]
-    custom_system_prompt = "Je bent een expert in het interpreteren van verzekeringsdocumenten. Bij het beantwoorden van vragen, gebruik de informatie uit de polisvoorwaarden. De gebruiker is een schadebehandelaar, geef altijd zo nuttig mogelijk antwoord. Wordt er bijvoorbeeld naar een maximale vergoeding gevraagd? Geef dan enkel het bedrag en waar het staat. Geef een accurate beschrijving van waar de gebruiker de informatie/context die jij hebt gebruikt kan vinden. Zorg ervoor dat je niet liegt of hallucineert over waar dit staat. Vraag: {user_question}" 
-    system_message_template = SystemMessagePromptTemplate.from_template(custom_system_prompt)
-
+    # PDF categorization and selection logic
+    # (Assuming the categorization logic is defined elsewhere in your script)
+    # ...
+    
     user_question = st.text_input("Stel een vraag over de polisvoorwaarden")
-    docs = None
-    
     if user_question:
-        # Perform document similarity search
+        # Assuming knowledge_bases and selected_pdf_path are defined
+        # and populated earlier in your script
+        knowledge_base = knowledge_bases[selected_pdf_path]
+        
         docs = knowledge_base.similarity_search(user_question)
-
-    # Check if documents are found and extract text from the first document
-    if docs and len(docs) > 0:
-        # Assuming the first document is the most relevant one
-        most_relevant_doc = docs[0]
-
-        # Extracting text based on your original implementation structure
-        # Update this part based on how the 'Document' object stores text in your original code
-        relevant_doc_content = getattr(most_relevant_doc, 'text', "Geen relevante inhoud gevonden in het document.")
-    else:
         relevant_doc_content = "Geen relevante inhoud gevonden in het document."
-
-    # Combine the relevant document content with the user question
-    combined_input = {"user_question": relevant_doc_content + "\n\n" + user_question}
-
-    # Invoke the chain with the combined input
-    response = chain.invoke(combined_input)
-    st.write(response.content)
-    
+        
+        if docs and len(docs) > 0:
+            most_relevant_doc = docs[0]
+            relevant_doc_content = getattr(most_relevant_doc, 'text', relevant_doc_content)
+        
+        response = chain.invoke(relevant_doc_content + "\n\n" + user_question)
+        st.write(response)
 
 if __name__ == '__main__':
     main()

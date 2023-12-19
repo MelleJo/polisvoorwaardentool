5619b7dceb7279b01a130e1bd2f004030aa0b2ba
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-12-19 09:10:23 +0000

    test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplat
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.callbacks import get_openai_callback
 from hashlib import sha256
-from langchain.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
 
@@ -97,45 +97,64 @@ def categorize_pdfs(pdf_list):
 def main():
     st.header("VA-Polisvoorwaardentool")
 
-    custom_prompt = (
-        "Je bent een expert in het interpreteren van verzekeringsdocumenten. "
-        "Bij het beantwoorden van vragen, gebruik de informatie uit de polisvoorwaarden. "
-        "De gebruiker is een schadebehandelaar, geef altijd zo nuttig mogelijk antwoord. "
-        "Wordt er bijvoorbeeld naar een maximale vergoeding gevraagd? Geef dan enkel het bedrag en waar het staat. "
-        "Geef een accurate beschrijving van waar de gebruiker de informatie/context die jij hebt gebruikt kan vinden. "
-        "Zorg ervoor dat je niet liegt of hallucineert over waar dit staat. "
-        "Vraag: {user_question}"
-    )
-
-    chain = (
-        {"user_question": RunnablePassthrough()}
-        | ChatPromptTemplate.from_template(custom_prompt)
-        | ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.2)
-        | StrOutputParser()
-    )
-
     # Set API key for OpenAI functionalities
     api_key = st.secrets["OPENAI_API_KEY"]
     os.environ["OPENAI_API_KEY"] = api_key
 
-    # PDF categorization and selection logic
-    # (Assuming the categorization logic is defined elsewhere in your script)
-    # ...
-    
+    # Load the PDFs and categorize them
+    pdf_dir = "preloaded_pdfs/"  # Adjust the path as needed
+    all_pdfs = [os.path.join(dp, f) for dp, dn, filenames in os.walk(pdf_dir) for f in filenames if f.endswith('.pdf')]
+    category_map = categorize_pdfs(all_pdfs)
+
+    # Let the user select a category and a specific PDF
+    categories = list(category_map.keys())
+    selected_category = st.selectbox("Kies een categorie:", categories)
+    available_pdfs = category_map[selected_category]
+    pdf_names = [os.path.basename(pdf) for pdf in available_pdfs]
+    selected_pdf_name = st.selectbox("Welke polisvoorwaarden wil je raadplegen?", pdf_names)
+
+    # Get the full path of the selected PDF
+    selected_pdf_index = pdf_names.index(selected_pdf_name)
+    selected_pdf_path = available_pdfs[selected_pdf_index]
+
+    # Process the selected PDF
+    with open(selected_pdf_path, "rb") as f:
+        pdf_reader = PdfReader(f)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text() or ""
+        text_splitter = CharacterTextSplitter(separator="\n", chunk_size=4000, chunk_overlap=1000, length_function=len)
+        chunks = text_splitter.split_text(text)
+
+    # Load embeddings and vector store for the selected PDF
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    # Define the chat model and prompt template
+    custom_prompt = ChatPromptTemplate.from_template(
+        "Je bent een expert in het interpreteren van verzekeringsdocumenten. "
+        "Bij het beantwoorden van vragen, gebruik de informatie uit de polisvoorwaarden. "
+        "De gebruiker is een schadebehandelaar, geef altijd zo nuttig mogelijk antwoord. "
+        "Vraag: {user_question}"
+    )
+    chat_model = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0.2)
+
+    # Get user input and generate response
     user_question = st.text_input("Stel een vraag over de polisvoorwaarden")
     if user_question:
-        # Assuming knowledge_bases and selected_pdf_path are defined
-        # and populated earlier in your script
-        knowledge_base = knowledge_bases[selected_pdf_path]
-        
         docs = knowledge_base.similarity_search(user_question)
         relevant_doc_content = "Geen relevante inhoud gevonden in het document."
-        
         if docs and len(docs) > 0:
             most_relevant_doc = docs[0]
             relevant_doc_content = getattr(most_relevant_doc, 'text', relevant_doc_content)
-        
-        response = chain.invoke(relevant_doc_content + "\n\n" + user_question)
+
+        chain = (
+            {"user_question": relevant_doc_content + "\n\n" + user_question}
+            | custom_prompt
+            | chat_model
+            | StrOutputParser()
+        )
+        response = chain.invoke()
         st.write(response)
 
 if __name__ == '__main__':

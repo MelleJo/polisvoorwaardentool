db51831ddcd08e3b87ce8858eaeffd5b844dd46a
MelleJo <141861480+MelleJo@users.noreply.github.com>
2023-12-27 15:30:29 +0000

    test

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def load_document(document_path):
         return index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 # Specify the directory where your PDFs are stored
-pdf_dir = "./polisvoorwaardentool/preloaded_pdfs/"
+pdf_dir = "./MelleJo/polisvoorwaardentool/preloaded_pdfs/"
 
 if not os.path.exists(pdf_dir):
     st.warning("PDF directory does not exist.")

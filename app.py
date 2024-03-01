2e608a3180f733b80c95f721dca458bc9065f9b0
MJ <mellejorna@gmail.com>
2024-03-01 16:18:45 +0100

    test btn voor download

--- app.py
+++ app.py
@@ -112,6 +112,17 @@ def display_search_results(search_results):
         if user_question:
             process_document(selected_document['path'], user_question)
 
+        if st.button('Download PDF'):
+            with open(selected_document['path'], "rb") as file:
+                btn = st.download_button(
+                    label="Download PDF",
+                    data=file,
+                    file_name=selected_document['title'],
+                    mime="application/octet-stream"
+                )
+
+    
+
 def main():
     st.title("Policy Conditions Tool - Version 1.1 (FAISS)")
     all_documents = get_all_documents()
